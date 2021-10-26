6977e092d8e6b0bff501d46e4ee596607f83a78c
Zing_Hoom <zing10@naver.com>
2021-10-27 04:02:04 +0900

    10-27-04시

--- 앱만들기/js/todo.js
+++ 앱만들기/js/todo.js
@@ -1,34 +1,47 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.getElementById("todo-list");
+const toDoList = document.getElementById("todo-list"); //ul
 
 
-function deleteToDo(){
- console.log("딜리트 누름");
+function deleteToDo(event){
+    //console.dir 상세정보
+                            //parentElement 는 element 의부모
+ const D_li = event.target.parentElement;
+ D_li.remove()
 }
 
+
+
+
 function paintToDo(newTodo){
-    const li = document.createElement("li");
-    const span = document.createElement("span");
+    const li = document.createElement("li"); //li 만들기
+    const span = document.createElement("span"); //span 만들기
     span.innerText = newTodo;
+
+
+    //버튼제작
     const button = document.createElement("button");
     button.innerText="X";
     button.addEventListener("click", deleteToDo);
-    //li 안에 span을 넣는다 버튼도
+
+
+    //li 안에 span을 넣는다 button도
     li.appendChild(span);
     li.appendChild(button);
 
+    // li 안에 span 과 button 을 넣은상태
 
-
-    // 실제로 넣기
+    // li를 실제로 넣기
     toDoList.appendChild(li);
 }
 
 
+
+
+
 function handleTodoSubmit(e){
-    e.preventDefault();
+    e.preventDefault();//새로고침 억제
     const newTodo = toDoInput.value;
-    console.log(newTodo, toDoInput.value);
     toDoInput.value = ""; //엔터지면 입력값 인풋에서 지우기
     paintToDo(newTodo);
 }
